8b7fb0d03ff5aed04bed7980ec2a47c1c4e60a98
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-08-26 21:57:20 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name+" is added.");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.Name +customer.Surname+" : "+customer.Id);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " is deleted.");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Smith";
+            customer1.Surname = "Johnson";
+            customer1.Id = 123465;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Williams";
+            customer2.Surname = "Brown";
+            customer2.Id = 123489;
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Garcia";
+            customer3.Surname = "Miller";
+            customer3.Id = 123447;
+
+            List<Customer> list = new List<Customer>();
+            list.Add(customer1);
+            list.Add(customer2);
+            list.Add(customer3);
+
+            foreach (var customer in list)
+            {
+                Console.WriteLine(customer.Name +" " customer.Surname + " : " + customer.Id);
+            }
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Delete(customer1);
+        }
+    }
+}
